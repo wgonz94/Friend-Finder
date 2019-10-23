537a245dbe8105899c3b5f1957da92b3b3093041
William Gonzales <wgonz94@yahoo.com>
2019-10-22 18:26:51 -0700

    added path route for survey.html

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+var path = require("path");
+
+module.exports = function(app){
+    app.get("/survey", function(req,res){
+        res.sendFile(path.join(_dirname, "../public/survey.html"));
+    });
+
+    
+}
