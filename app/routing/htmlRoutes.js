1dc92469e67d900bb5438a90602a3c1d5112e543
William Gonzales <wgonz94@yahoo.com>
2019-10-24 14:59:11 -0700

    changes to route for homepage

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    app.get("/home", function(req,res){
+    app.get("/", function(req,res){
         res.sendFile(path.join(__dirname,"../public/home.html"));
     });
 }
