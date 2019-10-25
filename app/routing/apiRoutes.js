d4284e13fd6e538e8d666a0823006fc4e6f7553c
William Gonzales <wgonz94@yahoo.com>
2019-10-24 18:01:23 -0700

    testing data for accuracy

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,15 +20,19 @@ module.exports = function(app) {
         // default index for friends, will change once comparing differences in scores
         let friendIndex = 0;
         // default score number for best match
-        let minScore = 40;
+        let minScore = 400000;
 
         //create for loop to generate friends list
         //create second for loop to pull scores individual to compare
         for(var i = 0; i < friends.length; i++){
             var totalDifference = 0;
             for (var j = 0; j < friends[i].scores.length; j++) {
+                // console.log('This User Score: ' + user.scores[j])
+                // console.log('This is Friends score' + friends[i].scores[j])
                 var between = Math.abs(user.scores[j] - friends[i].scores[j])
+                // console.log('this is between--' + between)
                 totalDifference += between;
+                // console.log(totalDifference)
             };
         //if total is smaller than Minimum, it will become new minimum
         //therefore, having chosen the "best match"
