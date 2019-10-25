d1e737ce6126e9492b2bc38ea597dc56c9dc0bce
William Gonzales <wgonz94@yahoo.com>
2019-10-24 21:33:08 -0700

    small tweaks to routing protocol

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var friends = require("../data/friends");
+var path = require("path")
 
 module.exports = function(app) {
     app.get("/api/friends", function(req,res){
@@ -12,44 +13,43 @@ module.exports = function(app) {
         //friend details(name, photo, and scores)
         var user = req.body;
 
-        //parse scores
-        for(var i = 0; i < user.scores.length; i++) {
-            user.scores[i] = parseInt(user.scores[i]);
-        }
+        
 
         // default index for friends,let friendIndex = 0; 
         //Another aspect looked, data-attr
         let matchName = "";
         let matchPhoto = "";
-
+        let matchArr = [];
         // default score number for comparison
         let totalDifference = 10000;
 
         //create for loop to generate friends list
-        //create second for loop to pull scores individual to compare
+        //create second for loop to pull scores to compare 
+        //parse values
         for(var i = 0; i < friends.length; i++){
             var differ = 0;
             for (var j = 0; j < user.scores.length; j++) {
-                // console.log('This User Score: ' + user.scores[j])
-                // console.log('This is Friends score' + friends[i].scores[j])
-                var between = Math.abs(user.scores[j] - friends[i].scores[j])
-                // console.log('this is between--' + between)
+                var between = Math.abs(parseInt(user.scores[j]) - parseInt(friends[i].scores[j]))
                 differ += between;
-                // console.log(totalDifference)
+                
             };
-
-        //if total is smaller than Minimum, it will become new minimum
-        //therefore, having chosen the "best match"
-            if(differ < totalDifference) {
-                // friendIndex = i;
-                totalDifference = differ;
-                matchName = friends[i].name;
-                console.log(matchName)
-                matchPhoto = friends[i].photo;
-                console.log(matchPhoto)
-            };
+    
+         
+         
+         if(differ < totalDifference) {
+            totalDifference = differ;
+            console.log(totalDifference)
+            matchName = friends[i].name;
+            console.log(matchName)
+            matchPhoto = friends[i].photo;
+            console.log(matchPhoto)
         };
 
+
+        };
+        //if total is smaller than Minimum, it will become new minimum
+        //therefore, having chosen the "best match"
+        
         //push new entry into friends list
         friends.push(user)
         //friends[friendIndex]
