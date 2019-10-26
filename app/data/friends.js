c76120ca979862bf68a7c8045869baf220758ccf
William Gonzales <wgonz94@yahoo.com>
2019-10-25 20:37:11 -0700

    altering data to match theme

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,101 +1,53 @@
 var friends = [ 
     {
-        name: "Jimmy Neutron",
-        photo: "https://nick-intl.mtvnimages.com/uri/mgid:file:docroot:nick.com:/nick-assets/shows/images/jimmy-neutron/characters/character_large_332x363_jimmy.jpg?quality=0.75&height=0&width=480&matte=true&crop=false",
+        name: "Dr.Frankenstein",
+        photo: "https://foundationsofliterarystudies.files.wordpress.com/2013/01/young-frankenstein.jpeg",
         scores: [
-            4,
+            5,
+            5,
             1,
             5,
+            4,
+            1,
+            1,
             3,
-            2,
-            5,
-            2,
-            2,
-            2,
-            5
-        ]
-    },
-    {
-        name: "Dexter Morgan",
-        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Dexter_Morgan.jpg/200px-Dexter_Morgan.jpg",
-        scores: [
             1,
-            2,
-            5,
-            2,
-            4,
-            1,
-            5,
-            2,
-            2,
-            5
-        ]
-    },
-    {
-        name: "Michael Scott",
-        photo: "https://pmctvline2.files.wordpress.com/2011/04/greatscott_april27_300110427100210.jpg?w=300&h=240",
-        scores: [
-            5,
-            5,
-            3,
-            5,
-            2,
-            1,
-            5,
-            5,
-            4,
             3
         ]
     },
     {
-        name: "Ron Burgundy",
-        photo: "https://vignette.wikia.nocookie.net/anchorman/images/1/10/Ron_burgundy.jpg/revision/latest?cb=20120329160125",
+        name: "Wolf Man",
+        photo: "https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2016/02/the_wolf_man_lon_chaney.jpg?itok=0C5xKBW6",
         scores: [
-            5,
-            4,
             2,
             4,
-            3,
-            1,
-            4,
+            5,
             5,
             3,
-            4
+            2,
+            5,
+            5,
+            1,
+            1
         ]
     },
     {
-        name: "Jessica Day",
-        photo: "https://uploads.wornontv.net/2016/12/jess-snowman-print-pajamas.jpg",
+        name: "Dracula",
+        photo: "https://nationalpostcom.files.wordpress.com/2017/03/bela.jpg?quality=80&strip=all&w=780",
         scores: [
             3,
-            4,
+            5,
             1,
             3,
-            2,
             5,
-            2,
-            4,
-            3,
-            5
-        ]
-    },
-    {
-        name: "Pam Beasly",
-        photo: "https://fashionista.com/.image/t_share/MTU3OTM4NjA2NjIxNTk5Mzc4/corporate-women-tv-show-wardrobes-outfits.jpg",
-        scores: [
-            2,
             5,
-            2,
-            4,
-            2,
             3,
-            2,
-            3,
-            3,
-            5
+            5,
+            1,
+            2
         ]
     }
-    
+   
 
 ]
 
